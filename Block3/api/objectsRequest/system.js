d14675ca21a31e8cc6dd9d47ebe205e1a98e49e3
Alex <43917289+Kazurov33@users.noreply.github.com>
2022-05-30 17:52:39 +0300

    Added swagger

--- Block3/api/objectsRequest/system.js
+++ Block3/api/objectsRequest/system.js
@@ -6,7 +6,32 @@ app.get("/api/system/all", async (req, res) => {
   // #swagger.description = 'Получение всех систем'
 
   try {
-    let systems = await prisma.System.findMany();
+    let systems = await prisma.system.findMany();
+    res.json(systems);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send(err.message);
+  }
+});
+
+app.post("/api/system/create", async (req, res) => {
+  // #swagger.tags = ['Systems']
+  // #swagger.description = 'Получение всех систем'
+
+  try {
+    let check = await prisma.system.findUnique({
+      where: { Key: String(req.body["Key"]) },
+    });
+    if (check) {
+      res.status(400).send("Key is not unique");
+      return;
+    }
+    let systems = await prisma.system.create({
+      data: {
+        Name: req.body["Name"],
+        Key: String(req.body["Key"]),
+      },
+    });
     res.json(systems);
   } catch (err) {
     console.error(err);
---
+++ Block3/api/swagger.js
@@ -0,0 +1,61 @@
+const swaggerAutogen = require("swagger-autogen")();
+
+const outputFile = "./swagger.json";
+const endpointsFiles = [
+  "./objectsRequest/event",
+  "./objectsRequest/system",
+  "./objectsRequest/users",
+];
+
+const doc = {
+  info: {
+    version: "1.0.2",
+    title: "Notify",
+    description: "API for notify service",
+  },
+  host: "localhost:3080",
+  basePath: "/",
+  schemes: ["https"],
+  consumes: ["application/json"],
+  produces: ["application/json"],
+  tags: [
+    {
+      name: "Auth",
+      description: "Методы авторизации",
+    },
+    {
+      name: "Systems",
+      description: "Методы авторизации",
+    },
+    {
+      name: "Events",
+      description: "Методы работы с машинами",
+    },
+  ],
+  securityDefinitions: {
+    AccessToken: {
+      type: "apiKey",
+      name: "x-access-token",
+      in: "header",
+    },
+  },
+  security: [
+    {
+      AccessToken: [],
+    },
+  ],
+  definitions: {
+    User: {
+      $PhoneNumber: "89997776655",
+      $Password: "Qwerty1",
+    },
+    AddUser: {
+      $Password: "Qwerty1",
+      $Login: "user",
+      $Email: "example@mail.ru",
+      $Role: 1,
+    },
+  },
+};
+
+swaggerAutogen(outputFile, endpointsFiles, doc);
